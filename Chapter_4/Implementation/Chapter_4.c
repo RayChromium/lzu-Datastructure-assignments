ba6a13eef50b7bb50a51f6304711fdeb966ff739
RayChromium <raychromium@outlook.com>
2018-10-24 21:53:23 +0800

    chapter 4

---
+++ Chapter_4/Implementation/Chapter_4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linkedlist.h"  //这里面有链表结点的定义
+ /**
+  * 第七周（第四章）作业的实现示例，此文件中包含main()
+  * 第三题：双栈共存问题
+  * 第四题：借助栈结构实现链表逆置
+  * 第五题：计算循环链表中的元素个数
+ */
+
+
+int main(int argc, char const *argv[])
+{
+    char op;
+    printf("A:第三题：双栈共存问题  B:第四题：借助栈结构实现链表逆置  C:第五题：计算循环链表中的元素个数");
+    while(op = getchar())
+    return 0;
+}
---
+++ Chapter_4/Implementation/linkedlist.h
@@ -0,0 +1,13 @@
+/**
+ * 用来存放链表结点的定义,以及一些函数的声明
+*/
+
+#ifndef LINKEDLIST_H__
+
+typedef struct node{
+    int data;
+    struct node *next;
+}node;
+
+#define LINKEDLIST_H__
+#endif /*linkedlist.h*/
