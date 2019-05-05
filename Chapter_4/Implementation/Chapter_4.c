03dc04036b0fb2e79d53d4c1e458f8684e622894
RayChromium <raychromium@outlook.com>
2019-05-05 13:37:40 +0800

    删库之后的备份恢复OTZ

--- Chapter_4/Implementation/Chapter_4.c
+++ Chapter_4/Implementation/Chapter_4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "linkedlist.h"  //这里面有链表结点的定义
+#include "linkedlist.h"  //这里面有链表结点的定义和函数的声明
  /**
   * 第七周（第四章）作业的实现示例，此文件中包含main()
   * 第三题：双栈共存问题
@@ -8,11 +8,18 @@
   * 第五题：计算循环链表中的元素个数
  */
 
-
 int main(int argc, char const *argv[])
 {
     char op;
-    printf("A:第三题：双栈共存问题  B:第四题：借助栈结构实现链表逆置  C:第五题：计算循环链表中的元素个数");
-    while(op = getchar())
+    printf("A:第三题：双栈共存问题  B:第四题：借助栈结构实现链表逆置  C:第五题：计算循环链表中的元素个数\n输入回车结束");
+    while((op = getchar())!= '\n'){
+        switch(op){
+            case 'a':case 'A':;
+            case 'b':case 'B':;
+            case 'c':case 'C':;
+            case 'd':case 'D':;
+            default:break;
+        }
+    }
     return 0;
 }
